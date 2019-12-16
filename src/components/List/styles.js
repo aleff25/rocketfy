909ba39a534bbc8057e640b157cb21562a04ba79
Aleff <aleff2594@gmail.com>
2019-12-15 21:25:26 -0300

    initial commit

---
+++ src/components/List/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 0 15px;
+  height: 100%;
+  flex: 0 0 320px;
+`;
+
+// flex-grow: 0;
+// flex-shrink: 0;
+// flex-basics: 320px;
